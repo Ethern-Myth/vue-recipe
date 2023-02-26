20a14241561d3a83644379b534de49bf7b411b5a
Ethern Myth <ethernmyth@gmail.com>
2023-02-26 15:46:10 +0200

    Updated Version

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Recipes from "./components/Recipes.vue";
+</script>
+
+<template>
+    <div class="container m-4">
+        <Recipes/>
+    </div>  
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Recipes.vue
@@ -0,0 +1,17 @@
+<script setup>
+// defineProps({
+//   msg: String,
+// })
+</script>
+
+<template>
+    <h3>Recipes</h3>
+    <div class="form-group mt-4">
+        <input type="text" name="search" id="search" class="form-control" placeholder="Search here" aria-describedby="helpId">
+    </div>
+    <a href="" class="btn btn-primary mt-2">Create</a>
+</template>
+
+<style scoped>
+
+</style>
