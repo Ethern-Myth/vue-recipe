9d159887ae2488903a9639e373d8f783a6fa357c
Ethern Myth <ethernmyth@gmail.com>
2023-02-27 15:50:39 +0200

    Updated Version

--- src/components/SingleRecipe.vue
+++ src/components/SingleRecipe.vue
@@ -1,5 +1,4 @@
 <script>
-import { ref, onMounted } from "vue";
 import { GET_Recipe_BY_id } from "../controllers/controller";
 
 export default {
@@ -11,9 +10,7 @@ export default {
     },
     created() {
         this.id = this.$route.params.id;
-        console.log(this.id);
         GET_Recipe_BY_id(this.id).then((res) => this.recipe = res)
-        console.log(this.recipe);
     }
 }
 
@@ -21,7 +18,7 @@ export default {
 
 <template>
     <div class="row">
-        <div class="col">
+        <div class="col vstack">
             <h3>{{ recipe.title }}</h3>
             <small>{{ recipe.tags }}</small>
             <small><i>{{ recipe.created_at }}</i></small>
@@ -29,9 +26,10 @@ export default {
         <div class="col hstack gap-2 justify-content-end">
             <router-link to="/save" class="btn btn-secondary">Edit</router-link>
             <button class="btn btn-danger">Delete</button>
+            <router-link to="/" class="btn btn-primary">Back</router-link>
         </div>
         <div class="mt-4">
-            <p></p>{{ recipe.body }}
+            <p>{{ recipe.body }}</p>
         </div>
     </div>
 </template>
